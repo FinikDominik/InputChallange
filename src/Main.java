dd08df539e845a93318a1d9ed940ce44be651f92
Dominik Fínik <finikdominik@gmail.com>
2022-08-01 12:01:39 +0200

    .

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage(5);
+    }
+
+    public static void inputThenPrintSumAndAverage(int number) {
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        while (scanner.hasNextInt()) {
+            sum += scanner.nextInt();
+            count++;
+        }
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((double) sum/count));
+
+        scanner.close();
+
+    }
+}
+
